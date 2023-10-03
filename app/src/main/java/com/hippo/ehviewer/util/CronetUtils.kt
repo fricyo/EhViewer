04db5be7cc043afcddc4bfa2bd338c60540957cc
FooIbar <118464521+FooIbar@users.noreply.github.com>
2023-10-03 22:36:37 +0800

    Enable cronet support for API 28

--- app/src/main/java/com/hippo/ehviewer/util/CronetUtils.kt
+++ app/src/main/java/com/hippo/ehviewer/util/CronetUtils.kt
@@ -3,4 +3,4 @@ package com.hippo.ehviewer.util
 import com.google.android.gms.net.CronetProviderInstaller
 
 val isCronetSupported: Boolean
-    get() = isAtLeastQ && CronetProviderInstaller.isInstalled()
+    get() = CronetProviderInstaller.isInstalled()
